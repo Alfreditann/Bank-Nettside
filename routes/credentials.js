c4ece1d6591a787d57f7fd4a6148412c4f889c75
Alfreditann <alfred.ulsberg@gmail.com>
2026-06-03 12:25:58 +0200

    fs

--- routes/credentials.js
+++ routes/credentials.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
 	res.render('credentials.ejs');
 });
+
+router.post('/', (req, res) => {
+	const { username, password, confirmPassword } = req.body || {};
+    console.log(username,password,confirmPassword)
+	return res.redirect('/credentials');
+});
+
 
 module.exports = router;
